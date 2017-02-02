f805ac80305fd17485476e030ac1f62c2a530156
Thahzan Mohomed <mthahzan@gmail.com>
2017-02-02 19:20:56 +0530

    Added query param for updated time filter

--- routes/postTypes.js
+++ routes/postTypes.js
@@ -8,14 +8,23 @@ const models = require('../models');
  * GET all PostTypes
  */
 router.get('/', (req, res, next) => {
+  const where = {};
+
+  // See if the user sent us the lastUpdate date so we can send back only the updates
+  if (req.query.lastUpdate && !isNaN(req.query.lastUpdate)) {
+    where.updatedAt = {
+      $gt: new Date(parseInt(req.query.lastUpdate)),
+    };
+  }
+
+  console.log('Where', where);
+
   models
     .PostType
-    .findAll()
-    .then((postTypes) => {
-      res.send({
-        results: postTypes,
-      });
+    .findAll({
+      where,
     })
+    .then((postTypes) => res.send({results: postTypes}))
     .catch(next);
 });
 
@@ -31,9 +40,7 @@ router.get('/:postTypeId', (req, res, next) => {
       },
       include: [models.Post],
     })
-    .then((postType) => {
-      res.send(postType);
-    })
+    .then((postType) => res.send(postType))
     .catch(next);
 });
 
